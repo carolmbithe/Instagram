8b31b8a0c173809da9589621d8dcb3a675695a3b
carolmbithe <carombithe97@gmail.com>
2018-11-07 16:48:47 +0300

    displaying model data

--- instagram/models.py
+++ instagram/models.py
@@ -1,27 +1,40 @@
 from django.db import models
+import datetime as dt
+
 
 # Create your models here.
 
 class Profile(models.Model):
-    profile_photo=models.ImageField(upload_to='profiles')
+    profile_photo=models.ImageField(upload_to='profiles',blank=True)
     profile_name=models.CharField(max_length =30)
     bio=models.TextField()
 
-    # def save_profile(self):
-    #     self.save()
-    # def delete_profile(self):
-    #     self.delete()
+    def save_profile(self):
+        self.save()
+    def delete_profile(self):
+        self.delete()
 
 class Image(models.Model):
-    image=models.ImageField(upload_to ='images/')
+    image=models.ImageField(upload_to ='images/', blank = True)
     image_name=models.CharField(max_length =30)
     image_caption=models.TextField()
-    profile=models.ForeignKey(Profile,on_delete=models.CASCADE)
+    profile=models.ForeignKey(Profile,on_delete=models.CASCADE,null=True)
     pub_date=models.DateTimeField(auto_now_add=True)
-    # likes=models.
-    # Comments
+    likes=models.IntegerField(blank=True,null=True)
+    Comments=models.TextField(blank=True,null=True)
 
-    # def save_image(self):
-    #     self.save()
-    # def delete_image(self):
-    #     self.delete()
+    def save_image(self):
+        self.save()
+    def delete_image(self):
+        self.delete()
+
+
+    @classmethod
+    def get_photos(cls):
+        photos=cls.objects.all()
+        return photos
+
+    @classmethod
+    def get_image_by_id(cls,image_id):
+        image=cls.objects.get(id=image_id)
+        return image
--- instagram/views.py
+++ instagram/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse,Http404
+from .models import Image
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    photos=Image.get_photos()
+    return render(request,'index.html',{"photos":photos})
+
+def imagedetails(request,image_id):
+    try:
+        image = Image.objects.get(id=image_id)
+    except ObjectDoesNotExist:
+         raise Http404()
+    return render(request,"image.html",{"image":image})
