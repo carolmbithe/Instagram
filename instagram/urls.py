e3ad881f8af242bc71b7f1b062dbba013d8cdcda
carolmbithe <carombithe97@gmail.com>
2018-11-12 13:05:24 +0300

    changes on the models

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,10 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-# urlpatterns=[
-# url('^$',views.index,name='index'),
-# url(r'^imagedetails/(\d+)',views.imagedetails,name ='imagedetails'),
-# url(r'^new/image$',views.new_image,name='new-image'),
+urlpatterns=[
+url('^$',views.index,name='index'),
+url(r'^imagedetails/(\d+)',views.imagedetails,name ='imagedetails'),
+url(r'^new/image$',views.new_image,name='new-image'),
 # url(r'^create/profile$',views.create_profile,name='create-profile'),
 # url(r'^profile/',views.profile,name='profile'),
 # url(r'^profiledetails/(\d+)',views.profiledetails,name ='profiledetails'),
